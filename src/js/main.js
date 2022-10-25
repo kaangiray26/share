ead4a3e3dcee236d9d32e780c3ec29fd4f791c79
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-10-26 00:58:35 +0200

    vue pwa

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from '/components/App.vue'
 import router from '/router'
-import '/registerServiceWorker';
 
 // Import our custom CSS
 import '/scss/styles.scss'
